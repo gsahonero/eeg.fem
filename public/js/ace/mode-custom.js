c49c5fc450dfcc1eb8f3405a096b4e7c2d262d1a
Josmar Suarez <luisjosmar@hotmail.com>
2019-10-14 10:56:13 -0400

    Now only the custom commands are highlighted

--- public/js/ace/mode-custom.js
+++ public/js/ace/mode-custom.js
@@ -58,31 +58,9 @@ ace.define("ace/mode/doc_comment_highlight_rules",["require","exports","module",
   
   var JavaScriptHighlightRules = function(options) {
       var keywordMapper = this.createKeywordMapper({
-          "variable.language":
-              "Array|Boolean|Date|Function|Iterator|Number|Object|RegExp|String|Proxy|"  + // Constructors
-              "Namespace|QName|XML|XMLList|"                                             + // E4X
-              "ArrayBuffer|Float32Array|Float64Array|Int16Array|Int32Array|Int8Array|"   +
-              "Uint16Array|Uint32Array|Uint8Array|Uint8ClampedArray|"                    +
-              "Error|EvalError|InternalError|RangeError|ReferenceError|StopIteration|"   + // Errors
-              "SyntaxError|TypeError|URIError|"                                          +
-              "decodeURI|decodeURIComponent|encodeURI|encodeURIComponent|eval|isFinite|" + // Non-constructor functions
-              "isNaN|parseFloat|parseInt|"                                               +
-              "JSON|Math|"                                                               + // Other
-              "this|arguments|prototype|window|document"                                 , // Pseudo
+          
           "keyword":
-              "experiment|id|beep|present|play|clear|wait|finish|ball|" +
-              "const|yield|import|get|set|async|await|" +
-              "break|case|catch|continue|default|delete|do|else|finally|for|function|" +
-              "if|in|of|instanceof|new|return|switch|throw|try|typeof|let|var|while|with|debugger|" +
-              "__parent__|__count__|escape|unescape|with|__proto__|" +
-              "class|enum|extends|super|export|implements|private|public|interface|package|protected|static",
-          "storage.type":
-              "const|let|var|function",
-          "constant.language":
-              "null|Infinity|NaN|undefined",
-          "support.function":
-              "alert",
-          "constant.language.boolean": "true|false"
+              "experiment|id|beep|present|play|clear|wait|finish|ball"
       }, "identifier");
       var kwBeforeRe = "case|do|else|finally|in|instanceof|return|throw|try|typeof|yield|void";
   

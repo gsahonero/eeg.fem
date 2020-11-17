da09447f8bad697f3a33c124d38b3c6316dfb669
Sergio Flores <sergiosfloresll@gmail.com>
2020-11-17 11:29:46 -0400

    Change in blink.js
    Fixes the connection with socketio

--- public/js/blink.js
+++ public/js/blink.js
@@ -1,20 +1,21 @@
-const socket = io();
-
-socket.on('connect', function () {
-    socket.on('dev', function(data){
-        let connection_information = data[2];
-        let total = 0;
-        for (let index = 0; index<=13; index=index+1){
-            total = total + connection_information[index];
-        }
-        let avg = total/14/4*100;
-        if (avg == 100){
-            document.body.style.background = "#038C33";
-        } else if ((avg < 100) && (avg >=75)){
-            document.body.style.background = "#F2C53D";
-        } else if (avg < 75){
-            document.body.style.background = "#D94E41";
-        }
+document.addEventListener('DOMContentLoadedOM', function(){
+    var socket = new io.connect('http://localhost:3000', {path: '/connection/eeg',reconnect: true});
+    socket.on('connect', function () {
+        socket.on('dev', function(data){
+            let connection_information = data[2];
+            let total = 0;
+            for (let index = 0; index<=13; index=index+1){
+                total = total + connection_information[index];
+            }
+            let avg = total/14/4*100;
+            if (avg == 100){
+                document.body.style.background = "#038C33";
+            } else if ((avg < 100) && (avg >=75)){
+                document.body.style.background = "#F2C53D";
+            } else if (avg < 75){
+                document.body.style.background = "#D94E41";
+            }
+        });
+        console.log('Connected to server');
     });
-    console.log('Connected to server');
-})
+}, false);

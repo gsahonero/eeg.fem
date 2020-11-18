0f6ac802a62dc19e111a37f0e93a45ca5e0e78a6
Sergio Flores <sergiosfloresll@gmail.com>
2020-11-17 22:30:30 -0400

    Changes Blink.js
    According to the connection level change the background, red, yellow and green to less than 75%, 75-99.9 and 100 respectively.

--- public/js/blink.js
+++ public/js/blink.js
@@ -1,15 +1,16 @@
-document.addEventListener('DOMContentLoadedOM', function(){
+document.addEventListener('DOMContentLoaded', function () {
     var socket = new io.connect('http://localhost:3000', {path: '/connection/eeg',reconnect: true});
     socket.on('connect', function () {
+        /*
+        * Refresh the connection information everytime 'dev' instruction is received
+        */
         socket.on('dev', function(data){
-            console.log('All: ',data);
             let connection_information = data[2];
             let total = 0;
             for (let index = 0; index<=13; index=index+1){
                 total = total + connection_information[index];
             }
             let avg = total/14/4*100;
-            console.log('Percent: ',avg);
             if (avg == 100){
                 document.body.style.background = "#038C33";
             } else if ((avg < 100) && (avg >=75)){
@@ -18,6 +19,5 @@ document.addEventListener('DOMContentLoadedOM', function(){
                 document.body.style.background = "#D94E41";
             }
         });
-        console.log('Connected to server');
     });
 }, false);

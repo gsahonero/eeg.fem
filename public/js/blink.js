77497e7a65a6f800b2a6837307a3e84a1ef9c475
Sergio Flores <sergiosfloresll@gmail.com>
2020-11-19 15:06:56 -0400

    Blink Detection
    Added .\public\py\blink.py, in blink.py do the mean of the eeg signals to verify if one person is blinking or not. also added the listen and emit part in start.js. In blink.js do the final operation to show in the blink.html.

--- public/js/blink.js
+++ public/js/blink.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let blink_action = document.getElementById('blink_action');
     var socket = new io.connect('http://localhost:3000', {path: '/connection/eeg',reconnect: true});
+    let img = document.querySelector('img');
     socket.on('connect', function () {
-        /*
-        * Refresh the connection information everytime 'dev' instruction is received
-        */
         socket.on('dev', function(data){
             let connection_information = data[2];
             let total = 0;
@@ -18,6 +17,11 @@ document.addEventListener('DOMContentLoaded', function () {
             } else if (avg < 75){
                 document.body.style.background = "#D94E41";
             }
+        });
+        socket.on('blink', function(data){
+            /* console.log(data); */
+            if (data.blink_OC>4300){blink_action.textContent = 'BLINK';}
+            else{blink_action.textContent = 'OPEN';}                
         });
     });
 }, false);
